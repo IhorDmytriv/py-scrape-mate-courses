eec96d08a5ee1cdb03a95d454550a617587449a7
Ihor Dmytriv <dmytriwihor@gmail.com>
2025-07-16 23:49:24 +0200

    Implement get_all_courses function

--- app/parse.py
+++ app/parse.py
@@ -4,6 +4,7 @@ import re
 import sys
 from dataclasses import dataclass, fields, astuple
 from typing import List
+from urllib.parse import urljoin
 
 from bs4 import BeautifulSoup
 from requests import Session
@@ -11,9 +12,10 @@ from requests import Session
 
 @dataclass
 class Course:
-    name: str
+    name: str = ""
     short_description: str = ""
     duration: str = ""
+    link: str = ""
 
 
 BASE_URL = "https://mate.academy/"
@@ -25,7 +27,6 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s",
     handlers=[
-        logging.FileHandler("app.log", encoding="utf-8"),
         logging.StreamHandler(sys.stdout)
     ],
 )
@@ -37,15 +38,45 @@ def get_page_soup(url: str) -> BeautifulSoup:
 
 
 def get_courses_from_page(page_soup: BeautifulSoup) -> List[Course]:
+    logging.info("Get courses from page")
+
     courses = []
-    courses_a = page_soup.find_all("a", href=re.compile(r"^/courses/"))
-    for tag_a in courses_a:
-        li = tag_a.find("li")
-        if li:
-            courses.append(li.text.strip())
+    courses_tag_a = page_soup.find_all("a", href=re.compile(r"^/courses/"))
     # Delete duplicates
-    courses = list(set(courses))
-    return [Course(name=course_name) for course_name in courses]
+    courses_tag_a = set(courses_tag_a)
+
+    for tag_a in courses_tag_a:
+
+        link = tag_a.get("href")
+        if "source" in link:
+            continue
+
+        li = tag_a.find("li")
+        if not li:
+            continue
+
+        course_link = (urljoin(BASE_URL, link))
+        course_name = (li.text.strip())
+
+        courses.append(Course(name=course_name, link=course_link))
+
+    return courses
+
+
+def parse_course_page(course: Course) -> Course:
+    logging.info(f"Parse course {course.name}")
+
+    page_soup = get_page_soup(course.link)
+
+    desc_tag = page_soup.find("p", class_=re.compile(r"Profession"))
+    if desc_tag:
+        course.short_description = desc_tag.text.strip()
+
+    duration_tag = page_soup.find("div", string=re.compile(r"місяці"))
+    if duration_tag:
+        course.duration = duration_tag.text.strip()
+
+    return course
 
 
 def write_courses_to_csv(courses: list[Course]) -> None:
@@ -58,8 +89,11 @@ def write_courses_to_csv(courses: list[Course]) -> None:
 
 def get_all_courses() -> list[Course]:
     page_soup = get_page_soup(BASE_URL)
-    return get_courses_from_page(page_soup)
-    # write_courses_to_csv(courses=courses)
+    courses = get_courses_from_page(page_soup)
+    list_parsed_courses = [parse_course_page(course) for course in courses]
+    logging.info("Done!")
+    write_courses_to_csv(list_parsed_courses)
+    return list_parsed_courses
 
 
 if __name__ == "__main__":
